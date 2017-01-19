b6c0808b45d19ab68293c563a787c2cdeaac611a
gaoshuming <gaoshuming.autohome.com.cn>
2017-01-19 10:24:28 +0800

    by abc

--- run.sh
+++ run.sh
@@ -3,18 +3,18 @@ workspace="/user/data/data.txt"
 conf=$workspace
 
 spark="/spark-2.0.2-with-hive/bin/spark-submit"
-exec="--queue q_guanggao.q_adalg 
+exec="--queue simrank
 --num-executors 200
 --driver-memory 20G
 --executor-memory 10G
 --executor-cores 8
 --master yarn 
---queue=q_guanggao.q_adalg
+--queue=simrank
 --conf spark.shuffle.memoryFraction=0.6
 --conf spark.storage.memoryFraction=0.7
 --conf spark.memory.storageFaction=0.5
 --class com.autohome.algo.simrank.Simrank
-/data/home/guanggao/semon/JAR/Simrank/simrank.jar $conf"
+/JAR/Simrank/simrank.jar $conf"
 echo $exec
 
 #--conf spark.default.parallelism=800
